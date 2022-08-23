b321ccc073cf084254f2edce75f2825996a7498f
Adriane Fagundes <adriane.cristina.fagundes@gmail.com>
2022-08-23 13:29:35 -0300

    feat: API
    Utilização de API do TMDB para utilizar dados de filmes no site.

--- projeto-rodrex/buscafilmes.js
+++ projeto-rodrex/buscafilmes.js
@@ -1,13 +1,48 @@
-const API_KEY = 'api_key=bfa52994c1d4057c319d2466a4493296';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 
-filmes(API_URL);
+const api_key = 'https://api.themoviedb.org/3/movie/popular?api_key=bfa52994c1d4057c319d2466a4493296'
+const img_url = 'https://image.tmdb.org/t/p/w500'
 
-function filmes(url) {
+const main = document.getElementById('main');
 
+
+pegarFilmes(api_key);
+
+function pegarFilmes(url) {
     fetch(url).then(res => res.json()).then(data => {
         mostrarFilmes(data.results);
+        console.log(data);
+    })  
+}
+
+function mostrarFilmes(data) {
+    main.innerHTML ='';
+
+    data.forEach(filmes => {
+        const {title, poster_path, vote_average, overview} = filmes;
+        const elementoFilme = document.createElement('div');
+        elementoFilme.classList.add('filmes');
+        elementoFilme.innerHTML = `
+            <div class="capa-filme">
+                <img src="${img_url+poster_path}" alt="${title}">
+            </div>
+            <div class="info-filme">
+                <h3>${title}</h3>
+                <span class="${corNota(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="resumo">
+                ${overview};
+            </div>
+        `
+        main.appendChild(elementoFilme);
     })
 }
 
+function corNota(vote_average) {
+    if(vote_average >= 8){
+        return 'verde'
+    } else if (vote_average >= 5){
+        return 'laranja'
+    }else {
+        return 'vermelho'
+    }
+}
